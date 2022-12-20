eedbaa4e100994e3658526fb2addb0da438633b8
110B01767Xiu <115057022+110B01767Xiu@users.noreply.github.com>
2022-12-21 03:12:20 +0800

    Create Class02.java

---
+++ Class02.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.String;
+public class Main {
+	public static void main(String args[]) {
+		Scanner scn = new Scanner(System.in);
+		String str;
+		str = scn.nextLine();
+		System.out.println(str.toUpperCase());
+	}
+}
